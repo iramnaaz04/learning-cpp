9dddf8bdc2709a2ad53f28e0f4cc2eb26655a5b1
iramnaaz04 <shaikhiramnaaz04@gmail.com>
2025-06-19 13:22:58 +0530

    Declare string

---
+++ string.cpp
@@ -0,0 +1,19 @@
+// String
+#include <iostream>
+int main(){
+     std::string name="iramnaaz";
+     std::cout <<"Hello, " <<name <<std::endl;
+     std::cout <<"The length of the name is: " <<name.length() <<std::endl;
+
+     std::string day="Thursday";
+     std::string month="October";
+     std::string food="Pav Bhaji";
+     std::string game="Badminton";
+     
+     std::cout <<day<<std::endl;
+     std::cout <<month<<std::endl;
+     std::cout <<food<<std::endl;
+     std::cout <<game<<std::endl;
+
+    return 0;
+}
