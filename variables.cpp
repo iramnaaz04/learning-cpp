ac495a5eba012c89e41c242726d090c6f803f977
iramnaaz04 <shaikhiramnaaz04@gmail.com>
2025-06-19 12:45:34 +0530

    Variables

---
+++ variables.cpp
@@ -0,0 +1,12 @@
+// Variables
+#include <iostream>
+int main(){
+    int x = 5;
+
+    std::cout <<"The value of x is: " <<x <<std::endl;
+    std::cout <<"The value of x multiplied by 2 is: " <<x*2 <<std::endl;
+
+    char grade = 'A';
+    std::cout <<"The grade is: " <<grade <<std::endl;
+    return 0;
+}
